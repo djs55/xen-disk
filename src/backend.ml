c08df23984f31d78a6a945c59ffe139832caa1be
David Scott <dave.scott@eu.citrix.com>
2014-01-23 21:35:02 +0000

    Replace MMAP with mirage-block-unix
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- src/backend.ml
+++ src/backend.ml
@@ -61,47 +61,13 @@ module DISCARD = struct
   *)
 end
 
-module MMAP = struct
-  include UNIMPLEMENTED
-
-  type t = string
-  let id x = failwith "id"
-  (*
-  (** Virtual disks backed by (possibly sparse) files accessed via mmap(2) *)
-  type t = int64 * Cstruct.t
-
-  let open_disk configuration =
-    let fd = Unix.openfile configuration.filename [ Unix.O_RDWR ] 0o0 in
-    let stats = Unix.LargeFile.fstat fd in
-    let mmap = Lwt_bytes.map_file ~fd ~shared:true () in
-    Unix.close fd;
-    return (Some (stats.Unix.LargeFile.st_size, Cstruct.of_bigarray mmap))
-
-  let size = fst
-
-  let read (_, mmap) buf offset_sectors len_sectors =
-    let offset_sectors = Int64.to_int offset_sectors in
-    let len_bytes = len_sectors * sector_size in
-    let offset_bytes = offset_sectors * sector_size in
-    Cstruct.blit mmap offset_bytes buf 0 len_bytes;
-    return ()
-
-  let write (_, mmap) buf offset_sectors len_sectors =
-    let offset_sectors = Int64.to_int offset_sectors in
-    let offset_bytes = offset_sectors * sector_size in
-    let len_bytes = len_sectors * sector_size in
-    Cstruct.blit buf 0 mmap offset_bytes len_bytes;
-    return () 
-    *)
-end
-
 (* Given a configuration, choose which backend to use *)
 let choose_backend { filename = filename; format = format } = match filename, format with
   | "", _ ->
     (module DISCARD: Storage.S)
   | _, Some "raw"
   | _, None ->
-    (module MMAP: Storage.S)
+    (module Block: Storage.S)
   | _, Some format ->
     failwith (Printf.sprintf "Unknown format: %s" format)
 
