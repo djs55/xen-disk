a2ed1df5a3ffa75f3962ab11851d4febfcdd160c
David Scott <dave.scott@eu.citrix.com>
2014-01-23 21:38:37 +0000

    Remove commented-out code
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- src/storage.ml
+++ src/storage.ml
@@ -11,22 +11,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *)
-(*
-let sector_size = 512
-*)
 type configuration = {
   filename: string;
   format: string option;
 }
 module type S = V1_LWT.BLOCK
   with type id = string
-(*
-module type S = sig
-  type t
-
-  val open_disk: configuration -> t option Lwt.t
-  val size: t -> int64
-  val read: t -> Cstruct.t -> int64 -> int -> unit Lwt.t
-  val write: t -> Cstruct.t -> int64 -> int  -> unit Lwt.t
-end
-*)
