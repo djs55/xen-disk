42641a26448230c4fc284ebb375d68b675e31947
David Scott <dave.scott@eu.citrix.com>
2014-02-02 11:16:17 +0000

    Print ring occupancy statistics on shutdown
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- src/main.ml
+++ src/main.ml
@@ -140,7 +140,10 @@ let main (vm: string) path backend =
     ) [ Sys.sigint; Sys.sigterm ];
 
   lwt () = S'.create name device in
-  lwt () = S'.run configuration.filename name device in 
+  lwt stats = S'.run configuration.filename name device in
+  Printf.fprintf stderr "# ring occupancy stats\n";
+  Printf.fprintf stderr "# slots-occupied frequency\n";
+  Array.iteri (fun i n -> if n <> 0 then Printf.fprintf stderr "%d %d\n" i n) stats.Blkback.ring_utilisation;
   S'.destroy name device
 
 let connect (common: Common.t) (vm: string) (path: string option) (backend: string option) =
